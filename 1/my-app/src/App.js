7eb250301428105673c963ba897a7cb2d6388f82
PopovichAlexey <79166620091@ya.ru>
2020-02-05 13:44:04 +0300

    Start of project 1-practice

---
+++ 1/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './intouch/main/main.jsx';
+import './App.css';
+
+
+function App() {
+  return (
+    <Main />
+  );
+}
+
+export default App;
---
+++ 1/my-app/src/navigation/navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navigation.css';
+
+const Navigation = () => {
+  return (
+    <div className="header">
+      <p>navigation</p>
+    </div>
+  );
+}
+
+export default Navigation;
