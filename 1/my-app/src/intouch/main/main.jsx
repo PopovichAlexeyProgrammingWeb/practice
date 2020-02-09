6870f7038997252d439586d2a2092feb96c98617
PopovichAlexey <79166620091@ya.ru>
2020-02-09 13:24:00 +0300

    Revert "hello chepuha"
    This reverts commit eac56844683c88a91237df89de750b15f7630260.

---
+++ 1/my-app/src/intouch/main/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './main.css';
+import Header from './../header/header.jsx';
+import Navigation from './../navigation/navigation.jsx';
+import Body from './../body/body.jsx';
+import Footer from './../footer/footer.jsx';
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className='header'><Header/></div>
+      <div className='navigation'><Navigation/></div>
+      <div className='mainContent'>
+        <div className='body'><Body/></div>
+      </div>
+      <div className='footer'><Footer/></div>
+    </div>
+);
+}
+
+
+export default Main;
